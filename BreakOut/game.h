c515a01883aa48374fdf5533f71d7a75b5859616
wnwoghd22 <53481297+wnwoghd22@users.noreply.github.com>
2022-01-07 13:28:37 +0900

    Setting Up

---
+++ BreakOut/game.h
@@ -0,0 +1,31 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Represents the current state of the game
+enum GameState {
+    GAME_ACTIVE,
+    GAME_MENU,
+    GAME_WIN
+};
+
+class Game
+{
+public:
+    // game state
+    GameState    State;
+    bool         Keys[1024];
+    unsigned int Width, Height;
+    // constructor/destructor
+    Game(unsigned int width, unsigned int height);
+    ~Game();
+    // initialize game state (load all shaders/textures/levels)
+    void Init();
+    // game loop
+    void ProcessInput(float dt);
+    void Update(float dt);
+    void Render();
+};
+#endif // !GAME_H
